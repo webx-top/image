c4589a3ccd6700ffe63443caf3568c209e8e503b
Hank Shen <swhbox@foxmail.com>
2020-04-13 21:50:24 +0800

    update

--- watermark.go
+++ watermark.go
@@ -13,6 +13,7 @@ import (
 	"strings"
 
 	"github.com/admpub/errors"
+	"golang.org/x/image/bmp"
 )
 
 // Pos 水印的位置
@@ -112,12 +113,31 @@ func (w *Watermark) MarkFile(path string) error {
 	return w.Mark(file, filepath.Ext(path))
 }
 
+func GetContentTypeByContent(buffer []byte) string {
+	// Use the net/http package's handy DectectContentType function. Always returns a valid
+	// content-type by returning "application/octet-stream" if no others seemed to match.
+	contentType := http.DetectContentType(buffer)
+	return contentType
+}
+
+func GetExtensionByContentType(contentType string) string {
+	switch {
+	case strings.Contains(contentType, "jpeg"):
+		return ".jpg"
+	case strings.Contains(contentType, "png"):
+		return ".png"
+	case strings.Contains(contentType, "bmp"):
+		return ".bmp"
+	case strings.Contains(contentType, "gif"):
+		return ".gif"
+	}
+}
+
 // Mark 将水印写入src中，由ext确定当前图片的类型。
 func (w *Watermark) Mark(src io.ReadWriteSeeker, ext string) error {
 	ext = strings.ToLower(ext)
 	var srcImg image.Image
 	var err error
-
 	switch ext {
 	case ".jpg", ".jpeg":
 		srcImg, err = jpeg.Decode(src)
@@ -125,6 +145,8 @@ func (w *Watermark) Mark(src io.ReadWriteSeeker, ext string) error {
 		srcImg, err = png.Decode(src)
 	case ".gif":
 		srcImg, err = gif.Decode(src)
+	case ".bmp":
+		srcImg, err = bmp.Decode(bytes.NewReader(data))
 	default:
 		return errors.WithMessage(ErrUnsupportedWatermarkType, ext)
 	}
